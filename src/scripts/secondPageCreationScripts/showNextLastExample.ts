f04f7dca8afd2853d54f077936ae03d8e92bc2b0
Aleksandra Cypko <olaaac25@gmail.com>
2020-12-19 22:06:16 +0100

    Fix an examples bug

--- src/scripts/secondPageCreationScripts/showNextLastExample.ts
+++ src/scripts/secondPageCreationScripts/showNextLastExample.ts
@@ -8,29 +8,33 @@ export function showNextLastExample (liCollection: HTMLCollection, text: string)
        const example = li.querySelector('.exampleMain span') as HTMLElement;  
        for (let phrase of textSplittedInPhrases) {
           if (phrase.includes(originalWord.value)) {
-             if (!examplesArray.includes(phrase)) {
-                examplesArray.push(phrase.replaceAll(originalWord.value, `<span class="emphasizedWord">${originalWord.value}</span>` ) + ".");
+             if (!examplesArray.includes(phrase.replaceAll(originalWord.value, `<span class="emphasizedWord">${originalWord.value}</span>` ).trim() + ".")) {
+                examplesArray.push(phrase.replaceAll(originalWord.value, `<span class="emphasizedWord">${originalWord.value}</span>` ).trim() + ".");
                 }
              } 
           } 
             let currentExample = examplesArray.indexOf(example.innerHTML); 
             if (currentExample < 0) {
-            example.innerHTML = examplesArray[0]; 
+            examplesArray[0] ? example.innerHTML = examplesArray[0] : example.innerHTML = '<input type="text">'; 
             }
              nextArrow!.addEventListener('click', () => {
              let currentExample = examplesArray.indexOf(example.innerHTML);
-             if (currentExample >= examplesArray.length - 1) {
-             example.innerHTML = examplesArray[0];
-             } else {
-             example.innerHTML = examplesArray[currentExample + 1];  
+             if (examplesArray.length > 1) {
+                  if (currentExample >= examplesArray.length - 1) {
+               example.innerHTML = examplesArray[0];
+               } else {
+               example.innerHTML = examplesArray[currentExample + 1];  
+             }
              }
           });
              lastArrow!.addEventListener('click', () => {
              let currentExample = examplesArray.indexOf(example.innerHTML);
-             if (currentExample === 0) {
-             example.innerHTML = examplesArray[examplesArray.length - 1];
-             } else {
-             example.innerHTML = examplesArray[currentExample - 1];  
+             if (examplesArray.length > 1) {
+               if (currentExample === 0) {
+               example.innerHTML = examplesArray[examplesArray.length - 1];
+               } else {
+               example.innerHTML = examplesArray[currentExample - 1];  
+             }  
              }
           })
       } 
--- src/scripts/secondPageCreationScripts/showNextLastSingleItem.ts
+++ src/scripts/secondPageCreationScripts/showNextLastSingleItem.ts
@@ -1,25 +1,26 @@
 // yet not working
 
 export function showNextLastSingleItem (li: HTMLElement, text: string) {
-    const textSplittedInPhrases: any = text.split(".");
-    const nextArrow = li.querySelector('.exampleArrowRight');
+     const textSplittedInPhrases: any = text.split(".");
+     const nextArrow = li.querySelector('.exampleArrowRight');
      const lastArrow = li.querySelector('.exampleArrowLeft');
      const originalWord = li.querySelector('.originalWord') as HTMLInputElement;  
-     const examplesArray: string[] = [];
+     let examplesArray: string[] = [];
      const example = li.querySelector('.exampleMain span') as HTMLElement;  
      for (let phrase of textSplittedInPhrases) {
         if (phrase.includes(originalWord.value)) {
-           if (!examplesArray.includes(phrase)) {
-              examplesArray.push(phrase.replaceAll(originalWord.value, `<span class="emphasizedWord">${originalWord.value}</span>` ) + ".");
+           if (!examplesArray.includes(phrase.replaceAll(originalWord.value, `<span class="emphasizedWord">${originalWord.value}</span>` ).trim() + ".")) {
+              examplesArray.push(phrase.replaceAll(originalWord.value, `<span class="emphasizedWord">${originalWord.value}</span>` ).trim() + ".");
               }
            } 
         } 
     let currentExample = examplesArray.indexOf(example.innerHTML); 
     if (currentExample < 0) {
-    example.innerHTML = examplesArray[0]; 
-    }
-    if (examplesArray.length > 1) {
-        nextArrow!.addEventListener('click', () => {
+      examplesArray[0] ? example.innerHTML = examplesArray[0] : example.innerHTML = '<input type="text">'; 
+      currentExample = 0;
+    } console.log(currentExample); console.log(examplesArray)
+      nextArrow!.addEventListener('click', () => {
+       let currentExample = examplesArray.indexOf(example.innerHTML);
        if (currentExample >= examplesArray.length - 1) {
        example.innerHTML = examplesArray[0];
        } else {
@@ -27,12 +28,13 @@ export function showNextLastSingleItem (li: HTMLElement, text: string) {
        }
     });
        lastArrow!.addEventListener('click', () => {
+       let currentExample = examplesArray.indexOf(example.innerHTML);
        if (currentExample === 0) {
-       example.innerHTML = examplesArray[examplesArray.length - 1];
+       example.innerHTML = examplesArray[examplesArray.length - 1] || examplesArray[0];
        } else {
        example.innerHTML = examplesArray[currentExample - 1];  
        }
     })
-    }  
+      
 }
  
